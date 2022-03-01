4b5b3143ce29a69cd9d8c7292d865cfd00a30965
Tom Roberts <t.roberts@kcl.ac.uk>
2022-03-01 14:49:37 +0000

    Updates dcm2nii_operator app.py

--- operators/dcm2nii_operator/application.py
+++ operators/dcm2nii_operator/application.py
@@ -1,5 +1,4 @@
-import copy
-import os
+import copy, os, subprocess
 
 from aide_sdk.inference.aideoperator import AideOperator
 from aide_sdk.model.dicom_image import DicomImage
@@ -7,36 +6,22 @@ from aide_sdk.model.dicom_series import DicomSeries
 from aide_sdk.model.operatorcontext import OperatorContext
 from aide_sdk.model.resource import Resource
 from aide_sdk.utils.file_storage import FileStorage
-from reportlab.pdfgen.canvas import Canvas
 
 
 class Dcm2Nii(AideOperator):
 
     def process(self, context: OperatorContext):
-        # origin_dicom = context.origin
-        #
-        # result = my_cool_stuff(origin_dicom)  # Your magic goes here
-        #
-        # file_manager = FileStorage(context)
-        # path = file_manager.save_dicom("my_results", result)
-        #
-        # result_dicom = Resource(format="dicom", content_type="result", file_path=path)
-        # context.add_resource(result_dicom)
-        # return context
-
         file_manager = FileStorage(context)
+        origin_dicom = context.origin
 
-        # load
-        dcmImageFile = r'/mnt/aide_data/DICOM/IM_0001'
-        dcmImage = DicomImage(dcmImageFile)
-        dcmData = dcmImage.load_dataset()
+        dcm_path = context.origin.file_path  # basically dcm_path should = /mnt/aide_data/DICOM
+        nii_path = ''                        # somewhere in the container (e.g: /mnt/nifti_data ?)
 
-        print(dcmImageFile)
-        print(dcmData.pixel_array.shape)
+        subprocess.run(["dcm2niix", "-z", "y", "-o", nii_path, "-f", "%t_%v_%s_%p", dcm_path])
 
-        canvas = Canvas('report.pdf')
-        canvas.drawString(300, 300, dcmImageFile)
-        canvas_pdf_data = canvas.getpdfdata()
-        file_path = file_manager.save_file(canvas_pdf_data, 'report.pdf')
-        context.add_resource(Resource(format='pdf', content_type='pixel_mean', file_path=file_path))
+        # not sure what to do here
+        # - load the nifti files in variable result_nii and send to Resource below?
+
+        result_nii = Resource(format="nifti", content_type="result_nii", file_path=dcm_path)  # file_path=dcm_path or file_path=nii_path?
+        context.add_resource(result_nii)
         return context
