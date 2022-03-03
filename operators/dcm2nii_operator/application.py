aee2e0692984d3c30d6b80020d684bea5e86d887
Tom Roberts <t.roberts@kcl.ac.uk>
2022-03-03 11:07:45 +0000

    Update application.py

--- operators/dcm2nii_operator/application.py
+++ operators/dcm2nii_operator/application.py
@@ -1,8 +1,6 @@
-import copy, os, subprocess
+import subprocess
 
 from aide_sdk.inference.aideoperator import AideOperator
-from aide_sdk.model.dicom_image import DicomImage
-from aide_sdk.model.dicom_series import DicomSeries
 from aide_sdk.model.operatorcontext import OperatorContext
 from aide_sdk.model.resource import Resource
 from aide_sdk.utils.file_storage import FileStorage
@@ -12,16 +10,13 @@ class Dcm2Nii(AideOperator):
 
     def process(self, context: OperatorContext):
         file_manager = FileStorage(context)
-        origin_dicom = context.origin
+        dicom_study = context.origin
 
-        dcm_path = context.origin.file_path  # basically dcm_path should = /mnt/aide_data/DICOM
-        nii_path = ''                        # somewhere in the container (e.g: /mnt/nifti_data ?)
+        dcm_path = dicom_study.file_path  # basically dcm_path should = /mnt/aide_data/DICOM
+        output_dir = ''                      # output directory in container (e.g: /mnt/nifti_data ?)
 
-        subprocess.run(["dcm2niix", "-z", "y", "-o", nii_path, "-f", "%t_%v_%s_%p", dcm_path])
+        subprocess.run(["dcm2niix", "-z", "y", "-o", output_dir, "-f", "%t_%v_%s_%p", dcm_path])
 
-        # not sure what to do here
-        # - load the nifti files in variable result_nii and send to Resource below?
-
-        result_nii = Resource(format="nifti", content_type="result_nii", file_path=dcm_path)  # file_path=dcm_path or file_path=nii_path?
+        result_nii = Resource(format="nifti", content_type="result_nii", file_path=output_dir)
         context.add_resource(result_nii)
         return context
