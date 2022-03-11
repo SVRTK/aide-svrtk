fb1f351297bde0ae8ad89c77c9cdb63564fe85ca
Tom Roberts <t.roberts@kcl.ac.uk>
2022-03-11 13:38:08 +0000

    Change naming convention to 'fetal'

---
+++ operators/fetal_brain_recon_operator/application.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+from aide_sdk.inference.aideoperator import AideOperator
+from aide_sdk.model.operatorcontext import OperatorContext
+from aide_sdk.model.resource import Resource
+from aide_sdk.utils.file_storage import FileStorage
+
+
+class FoetalBrainReconstructor(AideOperator):
+
+    def process(self, context: OperatorContext):
+        file_manager = FileStorage(context)
+
+        nii_stacks_resource = context.get_resources_by_type(format="nifti", content_type="nii_stacks")
+        nii_stacks_path = nii_stacks_resource.file_path     # Folder because added as folder in previous app
+        nii_3d_path = os.path.join(file_manager.mount_point, file_manager.write_location, 'nii_3d')
+
+        subprocess.run(["/home/scripts/docker-recon-brain-auto.bash", nii_stacks_path, "1", "-1"])
+
+        # TODO:
+        # Adjust docker-recon-brain-auto.bash to put SVR-output.nii.gz in new directory (nii_3d_path)
+
+        result_nii_3d = Resource(format="nifti", content_type="nii_3d", file_path=nii_3d_path)
+        context.add_resource(result_nii_3d)
+        return context
---
+++ operators/fetal_brain_recon_operator/run.bash
@@ -0,0 +1,1 @@
+docker run --rm --gpus all -v "/home/troberts/code/aide-svrtk/test_data/nii":/home/recon aide_3dfoetal_brain_recon_op /home/scripts/docker-recon-brain-auto.bash /home/recon/ 1 -1
