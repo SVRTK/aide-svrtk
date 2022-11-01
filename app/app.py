1efa8f2fa6705c2fb2e61c53d56687912c118ab4
Tom Roberts <t.roberts@kcl.ac.uk>
2022-11-01 17:09:41 +0000

    Update app.py with simple_image_op

--- app/app.py
+++ app/app.py
@@ -4,6 +4,16 @@
 
 import logging
 
+from simple_image_operator import SimpleImageOperator
+
+from monai.deploy.core import Application, resource
+from monai.deploy.core.domain import Image
+from monai.deploy.core.io_type import IOType
+from monai.deploy.operators.dicom_data_loader_operator import DICOMDataLoaderOperator
+from monai.deploy.operators.dicom_series_selector_operator import DICOMSeriesSelectorOperator
+from monai.deploy.operators.dicom_series_to_volume_operator import DICOMSeriesToVolumeOperator
+
+
 class FetalMri3dBrainApp(Application):
     """
     Motion-corrected 3D fetal brain MRI Application class
@@ -17,6 +27,25 @@ class FetalMri3dBrainApp(Application):
         """Operators go in here
         """
 
+        logging.info(f"Begin {self.compose.__name__}")
+
+        # Create the custom operator(s) as well as SDK built-in operator(s).
+        study_loader_op = DICOMDataLoaderOperator()
+        series_selector_op = DICOMSeriesSelectorOperator(Sample_Rules_Text)
+        series_to_vol_op = DICOMSeriesToVolumeOperator()
+
+        # Simple image operator
+        simple_image_op = SimpleImageOperator()
+
+        # Create operator processing pipeline
+        self.add_flow(study_loader_op, series_selector_op, {"dicom_study_list": "dicom_study_list"})
+        self.add_flow(
+            series_selector_op, series_to_vol_op, {"study_selected_series_list": "study_selected_series_list"}
+        )
+        self.add_flow(series_to_vol_op, simple_image_op, {"image": "image"})
+
+        logging.info(f"End {self.compose.__name__}")
+
 
 if __name__ == "__main__":
-    App(do_run=True)
+    FetalMri3dBrainApp(do_run=True)
