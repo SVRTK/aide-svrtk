15f6adcd5a9be34d769fc8c6daeb9889b2550c60
Tom Roberts <t.roberts@kcl.ac.uk>
2022-11-01 18:09:53 +0000

    Adds working operator – renamed RotateImageOperator
    Tested using `python app -i input -o output` where `input` dir contains single DICOM file

--- app/rotate_image_operator.py
+++ app/rotate_image_operator.py
@@ -1,4 +1,5 @@
-# Simple Image Operator
+# Rotate Image Operator
+# Simple operating to practice creating MAP
 
 import logging
 
@@ -6,40 +7,32 @@ import monai.deploy.core as md
 from monai.deploy.core import DataPath, ExecutionContext, Image, InputContext, IOType, Operator, OutputContext
 
 
-@md.input("image", DataPath, IOType.DISK)
+@md.input("image", Image, IOType.DISK)
 @md.output("saved_images_folder", DataPath, IOType.DISK)
-class SimpleImageOperator(Operator):
-    """Simple Image Operator
+class RotateImageOperator(Operator):
+    """Rotate Image Operator
+    Simple test operator
     """
 
     def compute(self, op_input: InputContext, op_output: OutputContext, context: ExecutionContext):
         from skimage.transform import rotate
         from skimage.io import imsave
+        from numpy import squeeze
 
         logging.info(f"Begin {self.compute.__name__}")
-
-        # data_in = op_input.get().asnumpy()
-        # if not data_in:
-        #     raise ValueError("Input image is not found.")
-        # data_out = rotate(data_in, angle=180)
 
         input_image = op_input.get("image")
         if not input_image:
             raise ValueError("Input image is not found.")
-        data_out = rotate(input_image, angle=180)
+        data_out = rotate(input_image._data, angle=180, preserve_range=True)
 
         logging.info(f"Performed rotation inside {self.compute.__name__}")
 
-        op_output_folder_name = DataPath("saved_images_folder")
-        op_output.set(op_output_folder_name, "saved_images_folder")
         op_output_folder_path = op_output.get("saved_images_folder").path
         op_output_folder_path.mkdir(parents=True, exist_ok=True)
         print(f"Operator output folder path: {op_output_folder_path}")
 
-        imsave(op_output_folder_path, data_out)
-
-        # output_folder = op_output.get().path
-        # output_path = output_folder / "final_output.png"
-        # imsave(output_path, data_out)
+        imsave(op_output_folder_path / "original_image.png", squeeze(input_image._data))
+        imsave(op_output_folder_path / "rotated_image.png", squeeze(data_out))
 
         logging.info(f"End {self.compute.__name__}")
