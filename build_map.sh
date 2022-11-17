2533719e63aab608ac94c9571980619652e7790f
Tom Roberts <t.roberts@kcl.ac.uk>
2022-11-17 13:13:28 +0000

    Adds build script to create MAP + extra 3rd party dcm2niix

---
+++ build_map.sh
@@ -0,0 +1,21 @@
+# cd ~/aide-svrtk
+# ensure venv running
+# ensure Docker running
+
+# Test MAP code locally
+python app -i input -o output
+
+# Test MAP with MONAI Deploy
+monai-deploy exec app -i input/ -o output/
+
+# Initial packaging of MAP
+monai-deploy package app --tag fetalsvrtk/aide:map-test -l DEBUG
+
+# Push to DockerHub
+docker push fetalsvrtk/aide:map-test
+
+# Build 3rd-party software on top of MAP
+docker build -t fetalsvrtk/aide:map-test-extra app/
+
+# Test MAP-Extra with MONAI Deploy
+monai-deploy run fetalsvrtk/aide:map-test-extra input output
