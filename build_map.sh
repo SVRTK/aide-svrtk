f43278c46127d0cce95769f91b5a3827af857dad
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-06 15:36:04 +0000

    Update build_map.sh

--- build_map.sh
+++ build_map.sh
@@ -15,11 +15,16 @@ monai-deploy package app --tag fetalsvrtk/aide:map-test -l DEBUG
 docker push fetalsvrtk/aide:map-test
 
 # Build 3rd-party software on top of MAP
-docker build -t fetalsvrtk/aide:map-test-extra app/
+# CPU mode
+git checkout map
+docker build -t fetalsvrtk/aide:map app/
+# GPU mode
+git checkout map-gpu-on
+docker build -t fetalsvrtk/aide:map-gpu app/
 
 # Test MAP-Extra with MONAI Deploy
-monai-deploy run fetalsvrtk/aide:map-test-extra input output
+monai-deploy run fetalsvrtk/aide:map input output
 
 # Optional: Test scripts within Docker container
 # - On DGX:
-docker run -it --rm -v /home/troberts/code/aide-svrtk/input/nii_stacks:/home/recon --entrypoint /bin/bash fetalsvrtk/aide:map-test-extra
+docker run -it --rm -v /home/troberts/code/aide-svrtk/input/nii_stacks:/home/recon --entrypoint /bin/bash fetalsvrtk/aide:map
