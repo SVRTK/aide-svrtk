d25158bc41e0fe61edb5581d01cb75b2b6556ea8
Tom Roberts <t.roberts@kcl.ac.uk>
2022-12-16 10:01:08 +0000

    Removes old AIDE V0.2 files

--- operators/nii2dcm_operator/main.py
+++
@@ -1,5 +0,0 @@
-from aide_sdk.application import AideApplication
-from application import Nii2Dcm
-
-if __name__ == "__main__":
-    AideApplication.start(operator=Nii2Dcm())
--- operators/nii2dcm_operator/run.bash
+++
@@ -1,1 +0,0 @@
-docker run -it -v "/home/troberts/code/aide-svrtk/test_env":/home/ aide_3dfetal_nii2dcm_op /bin/bash
